14f89f925e71fa1a18b141041318662bb3b4e960
peddireddyrohith <153419049+peddireddyrohith@users.noreply.github.com>
2026-07-22 12:13:06 +0530

    Time: 1 ms (99.92%), Space: 46.6 MB (79.1%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -8,23 +8,36 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+class Solution {//recurision method
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b)->a.val-b.val);
-        for(ListNode node:lists){
-            if(node!=null){
-                pq.offer(node);
-            }
-        }
+      if(lists==null || lists.length == 0) return null;
+      return divideList(lists,0,lists.length-1);  
+    }
+    public ListNode divideList(ListNode []lists,int left,int right){
+        if(left==right)return lists[left];
+        int mid = left+(right-left)/2;
+        ListNode l1 = divideList(lists,left,mid);
+        ListNode l2 = divideList(lists,mid+1,right);
+        return merge(l1,l2);
+    }
+    public ListNode merge(ListNode a, ListNode b){
         ListNode dummy = new ListNode(0);
         ListNode tail = dummy;
-        while(!pq.isEmpty()){
-            ListNode node=pq.poll(); 
-            tail.next = node;
-            tail = tail.next;
-            if(node.next != null){
-                pq.offer(node.next);
+        while(a!=null&&b!=null){
+            if(a.val<=b.val){
+                tail.next= a;
+                a= a.next;
+            }else{
+                tail.next = b;
+                b = b.next;
             }
-        }return dummy.next;
+            tail = tail.next;
+        }
+        if(a!=null){
+            tail.next = a;
+        }else{
+            tail.next =b;
+        }
+        return dummy.next;
     }
 }
