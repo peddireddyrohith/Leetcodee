13205623700808516a06fbd939e2b389cfcb3506
peddireddyrohith <153419049+peddireddyrohith@users.noreply.github.com>
2026-08-02 00:12:41 +0530

    Time: 3 ms (4.6%), Space: 42.9 MB (11.49%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,14 +1,16 @@
 class Solution {
     public int reverse(int x) {
-        long res = 0;
-        while(x!=0){
-            int rem  = x%10;
-            res = res*10 +rem ;
-            if(res>Integer.MAX_VALUE || res< Integer.MIN_VALUE){
-                return 0;
-            }
-            x = x/10;
+        int res = 0;
+        boolean isNegative = x < 0;
+        String strX = String.valueOf(Math.abs(x));
+        StringBuilder sb = new StringBuilder(strX).reverse();
+        
+        try {
+            res = Integer.parseInt(sb.toString());
+        } catch (NumberFormatException e) {
+            return 0;
         }
-        return (int)res;
+        
+        return isNegative ? -res : res;       
     }
 }
