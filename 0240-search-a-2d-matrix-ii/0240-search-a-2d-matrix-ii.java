3cad4f168c9355219ae0bc3594facf0ac328c676
peddireddyrohith <153419049+peddireddyrohith@users.noreply.github.com>
2026-08-04 23:57:16 +0530

    Time: 3 ms (88.36%), Space: 48.2 MB (65.66%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,14 +1,19 @@
-class Solution {
+public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-
-                if (matrix[i][j] == target)
-                    return true;
+        if(matrix == null || matrix.length < 1 || matrix[0].length <1) {
+            return false;
+        }
+        int col = matrix[0].length-1;
+        int row = 0;
+        while(col >= 0 && row <= matrix.length-1) {
+            if(target == matrix[row][col]) {
+                return true;
+            } else if(target < matrix[row][col]) {
+                col--;
+            } else if(target > matrix[row][col]) {
+                row++;
             }
         }
-
         return false;
     }
 }
