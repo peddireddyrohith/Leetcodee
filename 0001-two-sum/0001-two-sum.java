3c0ce162f88df1999f857da6ac9d5bc96626fbc1
peddireddyrohith <153419049+peddireddyrohith@users.noreply.github.com>
2026-09-18 01:08:15 +0530

    Time: 5 ms (49.74%), Space: 46.7 MB (93.59%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,14 +3,19 @@ class Solution {
         Map<Integer, Integer> numMap = new HashMap<>();
         int n = nums.length;
 
+        // Build the hash table
         for (int i = 0; i < n; i++) {
-            int complement = target - nums[i];
-            if (numMap.containsKey(complement)) {
-                return new int[] { numMap.get(complement), i };
-            }
             numMap.put(nums[i], i);
         }
 
-        return new int[] {}; // No solution found
+        // Find the complement
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement) && numMap.get(complement) != i) {
+                return new int[]{i, numMap.get(complement)};
+            }
+        }
+
+        return new int[]{}; // No solution found
     }
 }
